79b3a3a284f4d40f7ce90633d2da80239f4b7454
Kevin <kevinskievanherwijnen@gmail.com>
2023-03-05 02:12:39 +0100

    Share, toolbar

--- FoodNutrition/app/src/main/java/com/example/foodnutrition/DetailFragment.java
+++ FoodNutrition/app/src/main/java/com/example/foodnutrition/DetailFragment.java
@@ -1,11 +1,15 @@
 package com.example.foodnutrition;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import androidx.fragment.app.Fragment;
 
@@ -22,13 +26,24 @@ public class DetailFragment extends Fragment {
         View view = inflater.inflate(R.layout.detail_fragment, container, false);
         titleTextView = view.findViewById(R.id.titleTextView);
         instructionTextView = view.findViewById(R.id.instructionsTextView);
-//        imageView = view.findViewById(R.id.imageView);
+        ImageButton button = (ImageButton) view.findViewById(R.id.shareButton);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                shareRecipe(view);
+            }
+        });
+
+
+        Toolbar shareToolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        MainActivity mainActivity = (MainActivity) getActivity();
+//        mainActivity.setSupportActionBar(shareToolbar);
 
         Bundle argument = getArguments();
         if(argument != null) {
-//            String dishTitle = argument.getString("title");
             Dish dish = argument.getParcelable(DISH_PARCEL, Dish.class);
-//            Dish dish = new Dish(dishTitle, "todo: show info of dish here!"); // TODO get data from API with this dish title
             setDish(dish);
         }
 
@@ -47,5 +62,14 @@ public class DetailFragment extends Fragment {
         titleTextView.setText(dish.getTitle());
         instructionTextView.setText(Html.fromHtml(dish.getInstructions(), Html.FROM_HTML_MODE_COMPACT));
 
+    }
+
+    public void shareRecipe(View view) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, "Here is the recipe to make " + dish.getTitle() + " ! " + Html.fromHtml(dish.getInstructions()));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Recipe for " + dish.getTitle());
+        Intent shareIntent = Intent.createChooser(intent, "Share");
+        startActivity(shareIntent);
     }
 }
